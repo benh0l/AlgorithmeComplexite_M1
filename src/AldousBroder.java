04fc6398113bc7db3b09447768556a6152d02cbe
benhol <benholpgm@gmail.com>
2018-12-28 17:05:01 +0100

    Question 5 : check

---
+++ src/AldousBroder.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class AldousBroder {
+
+    public Graph graphe;
+
+    public AldousBroder(Graph g){
+        this.graphe = aldousBroder(g);
+    }
+
+    public Graph aldousBroder(Graph g){
+        Graph graph = new Graph(g.vertices());
+        Random random = new Random();
+        boolean visite[] = new boolean[g.vertices()];
+        ArrayList<Edge> voisin = new ArrayList<Edge>();
+
+        int sommet = random.nextInt(g.vertices());
+        visite[sommet] = true;
+        while(!tousVisite(visite)){
+            voisin = g.adj(sommet);
+            int alea = random.nextInt(voisin.size());
+            int sommet2 = voisin.get(alea).other(sommet);
+            if(visite[sommet2] == false){
+                if(sommet < sommet2)
+                    graph.addEdge(new Edge(sommet, sommet2));
+                else
+                    graph.addEdge(new Edge(sommet2, sommet));
+                visite[sommet2] = true;
+                //voisin.get(sommet2).used = true;
+            }
+            sommet = sommet2;
+        }
+
+        return graph;
+    }
+
+    public boolean tousVisite(boolean[] tab){
+        for(int i = 0; i < tab.length; i++){
+            if(tab[i] == false){
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- src/TestPersos.java
+++ src/TestPersos.java
@@ -63,6 +63,55 @@ public class TestPersos {
         }
         System.out.println(total);
 
+
+        //QUESTION 5
+        arbre = new int[8];
+        for(int j = 0;j < arbre.length; j++){
+            arbre[j] = 0;
+        }
+        for(int i = 0; i < 1000000; i++){
+            AldousBroder ab = new AldousBroder(Graph.example());
+            ArrayList<Edge> aretes = ab.graphe.edges();
+            //Tri afin d'organiser les aretes dans un certain ordre et simplifier le comptage des arbres ensuite
+            aretes.sort(new Comparator<Edge>() {
+                @Override
+                public int compare(Edge o1, Edge o2) {
+                    if(o1.from == o2.from){
+                        return o1.to - o2.to;
+                    }else{
+                        return o1.from - o2.from;
+                    }
+                }
+            });
+
+            //On vérifie de quel arbre il s'agit
+            if(aretes.get(0).from == 0 && aretes.get(0).to == 1 && aretes.get(1).from == 0 && aretes.get(1).to == 3 && aretes.get(2).from == 1 && aretes.get(2).to == 2){
+                arbre[0]++;
+            }else if(aretes.get(0).from == 0 && aretes.get(0).to == 1 && aretes.get(1).from == 0 && aretes.get(1).to == 2 && aretes.get(2).from == 0 && aretes.get(2).to == 3){
+                arbre[1]++;
+            }else if(aretes.get(0).from == 0 && aretes.get(0).to == 1 && aretes.get(1).from == 1 && aretes.get(1).to == 2 && aretes.get(2).from == 1 && aretes.get(2).to == 3){
+                arbre[2]++;
+            }else if(aretes.get(0).from == 0 && aretes.get(0).to == 2 && aretes.get(1).from == 0 && aretes.get(1).to == 3 && aretes.get(2).from == 1 && aretes.get(2).to == 3){
+                arbre[3]++;
+            }else if(aretes.get(0).from == 0 && aretes.get(0).to == 3 && aretes.get(1).from == 1 && aretes.get(1).to == 2 && aretes.get(2).from == 1 && aretes.get(2).to == 3) {
+                arbre[4]++;
+            }else if(aretes.get(0).from == 0 && aretes.get(0).to == 2 && aretes.get(1).from == 1 && aretes.get(1).to == 2 && aretes.get(2).from == 1 && aretes.get(2).to == 3) {
+                arbre[5]++;
+            }else if(aretes.get(0).from == 0 && aretes.get(0).to == 2 && aretes.get(1).from == 0 && aretes.get(1).to == 3 && aretes.get(2).from == 1 && aretes.get(2).to == 2) {
+                arbre[6]++;
+            }else if(aretes.get(0).from == 0 && aretes.get(0).to == 1 && aretes.get(1).from == 0 && aretes.get(1).to == 2 && aretes.get(2).from == 1 && aretes.get(2).to == 3) {
+                arbre[7]++;
+            }
+        }
+        //On écrit les résulats
+        total = 0;
+        System.out.println("\n\n\nQUESTION 5 : \n Résultat :");
+        for(int ii = 0; ii < arbre.length;ii++) {
+            System.out.println("Arbre "+ (ii+1)+" : "+arbre[ii]+" / 1 000 000");
+            total += arbre[ii];
+        }
+        System.out.println(total);
+
     }
 
 }
